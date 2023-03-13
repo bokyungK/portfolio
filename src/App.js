e6a9f19e6dec6dedbb65c88d2e4bd2caf1c9945c
freebob2b <freebob2b@gmail.com>
2023-03-13 18:12:53 +0900

    fix router error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <div className="App">
           <div className="wrapper">
             <Header></Header>
