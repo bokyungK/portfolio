75c5ae7d7e0026520c4e3da621d828fef37d2073
freebob2b <freebob2b@gmail.com>
2023-03-22 20:09:12 +0900

    change stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -8,7 +8,7 @@ function Stats() {
                     <li className="skill">
                         <h2>HTML</h2>
                         <div className="description">
-                            <span>Semantic Web 구현</span>
+                            <span>Semantic tag</span>
                             <span>meta tag를 이용한 검색 엔진 최적화</span>
                         </div>
                     </li>
@@ -16,36 +16,22 @@ function Stats() {
                         <h2>CSS</h2>
                         <div className="description">
                             <span>styled component</span>
-                            <span>반응형</span>
+                            <span>반응형 웹</span>
                         </div>
                     </li>
                     <li className="skill">
                         <h2>Javascript</h2>
                         <div className="description">
                             <span>Vanilla js</span>
+                            <span>fetch/axios 비동기 통신 구현</span>
                         </div>
                     </li>
                     <li className="skill">
                         <h2>React</h2>
                         <div className="description">
-                            <span>함수형 컴포넌트 경험</span>
+                            <span>함수형 컴포넌트</span>
                             <span>Hooks(useState, useEffect, useCallback, Router ..) 사용 경험</span>
                             <span>recoil(상태 관리 라이브러리) 사용 경험</span>
-                        </div>
-                    </li>
-                    <li className="skill">
-                        <h2>
-                            Express
-                        </h2>
-                        <div className="description">
-                            <span>Rest Api 통신 구현</span>
-                        </div>
-                    </li>
-                    <li className="skill">
-                        <h2>AWS</h2>
-                        <div className="description">
-                            <span>EC2 서버 배포</span>
-                            <span>RDS(MYSQL) 서버와 연결</span>
                         </div>
                     </li>
                 </ol>
